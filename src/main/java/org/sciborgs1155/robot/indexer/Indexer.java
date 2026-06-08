2518f882d1c7caa5511dcd32c4ee96be45cc799a
StarlitHaruka <harukasmain@gmail.com>
2026-06-08 16:45:22 -0400

    base done

--- src/main/java/org/sciborgs1155/robot/indexer/Indexer.java
+++ src/main/java/org/sciborgs1155/robot/indexer/Indexer.java
@@ -1,10 +1,72 @@
 package org.sciborgs1155.robot.indexer;
 
+import static edu.wpi.first.units.Units.Amps;
+
+import org.sciborgs1155.lib.Beambreak;
 import org.sciborgs1155.lib.SimpleMotor;
+import org.sciborgs1155.robot.Robot;
+import org.sciborgs1155.robot.hopper.HopperConstants;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class Indexer {
 
-    private static SimpleMotor motor;
-    private 
+    private final SimpleMotor motor;
+    private final Beambreak beambreak;
+    public Trigger blocked;
+
+    private Indexer create() {
+        
+        return Robot.isReal() 
+        ? new Indexer(real(), Beambreak.real(org.sciborgs1155.robot.Ports.Hopper.BEAMBREAK)) 
+        : new Indexer(none(), Beambreak.none());
+
+    }
+
+    /**
+     * creates a new none hopper.
+     * @return a none simplemotor object 
+     */
+    private static SimpleMotor none() {
+        return SimpleMotor.none();
+    }
+
+    /**
+     * creates a new simplemotor hopper.
+     * @return a talon simplemotor object 
+     */
+    private static SimpleMotor real() {
+        TalonFX motor = new TalonFX(0);
+        TalonFXConfiguration config = new TalonFXConfiguration();
+
+        config.CurrentLimits.SupplyCurrentLimit = HopperConstants.CURRENT_LIMIT.in(Amps);
+        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+        return SimpleMotor.talon(motor, config);
+
+    }
+
+    private Indexer(SimpleMotor motor, Beambreak beambreak) {
+        this.motor = motor;
+        this.beambreak = beambreak;
+        this.blocked = new Trigger(() -> beambreak.get());
+
+    }
+
+    public Command run(double v) {
+        return Commands.run(() -> motor.set(v));
+    }
+
+    public Command stop() {
+        return run(0);
+    }
+
+
     
 }
