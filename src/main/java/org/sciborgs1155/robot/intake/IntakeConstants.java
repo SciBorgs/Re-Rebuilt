d17fa84ae6a57395dddf13c208ff53b337e8aa76
Stephanie Zhu <Stephaniezhu07@gmail.com>
2026-06-15 04:11:10 -0400

    intake 1

--- src/main/java/org/sciborgs1155/robot/intake/IntakeConstants.java
+++ src/main/java/org/sciborgs1155/robot/intake/IntakeConstants.java
@@ -1,1 +1,12 @@
+package org.sciborgs1155.robot.intake;
 
+import static edu.wpi.first.units.Units.Amps;
+
+import edu.wpi.first.units.measure.Current;
+
+public class IntakeConstants {
+    public static final Current CURRENT_LIMIT = Amps.of(30);
+    public static final double GEARING = 2.0; 
+    public static final double INTAKE_POWER = 0.5;
+    
+}
